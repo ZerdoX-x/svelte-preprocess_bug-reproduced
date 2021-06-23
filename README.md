e0ae3894179094e605221a122d9a78355d96c28d
ZerdoX-x <zerdox.cool4@gmail.com>
2021-06-23 15:11:20 +0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+[vite-plugin-svelte/issues/66](https://github.com/sveltejs/vite-plugin-svelte/issues/66)
+Bug reproduction
---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<ul>
+  <li>
+    <span class="text">text</span>
+  </li>
+</ul>
+
+<style lang="stylus">
+@media screen and (min-width: 0px) {
+  ul :global(.text) {
+    background: red;
+  }
+  /*
+  ul :global([class~="text"]) {
+    background: red;
+  }
+  */
+}
+</style>
